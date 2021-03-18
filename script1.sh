da27b3da68cc34202b63356a19af93bee5c6069c
HPL <klymash.vit@gmail.com>
2021-03-18 13:23:34 +0200

    Script 1

---
+++ script1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+p2=$(python2 --version)
+p3=$(python3 --version)
+echo -e "In my laptop I have a $p2 version and a $p3 version.\nWhen it comes to Python version 2 vs. 3 today, Python 3 is the outright winner. That's because Python 2 won't be available after 2020. Mass Python 3 adoption is the clear direction of the future.\nAfter considering declining support for Python 2 programming language and added benefits from upgrades to Python 3, it is always advisable for a new developer to select Python version 3. However, if a job demands Python 2 capabilities, that would be an only compelling reason to use this version. " 
